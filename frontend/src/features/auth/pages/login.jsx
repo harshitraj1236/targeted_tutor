a4bd55a22bcd370d85f678d69dd050e9e9e14853
Harshit Raj <harshitraj1236@gmail.com>
2026-07-14 21:54:44 +0530

    Update login page and frontend dependencies

--- frontend/src/features/auth/pages/login.jsx
+++ frontend/src/features/auth/pages/login.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { useState } from 'react'
 import { useNavigate, Link } from 'react-router'
 import { useAuth } from '../hooks/useAuth'
+import gsap from 'gsap'
+import { useGSAP } from '@gsap/react'
 
 const login = () => {
 
@@ -9,24 +11,37 @@ const login = () => {
     const [username, setUsername] = useState("")
     const [password, setPassword] = useState("")
     const navigate = useNavigate()
+    const containerRef = useRef(null)
+
+    useGSAP(()=>{
+        if(!loading){
+            gsap.from(".animate",{
+                y: 30,
+                opacity: 0,
+                duration: 1,
+                stagger: 0.1,
+                ease: "power3.out"
+            })
+        }
+    }, { scope: containerRef, dependencies: [loading] } )
 
     if(loading){
         return (<main><h1>Loading.......</h1></main>)
     }
 
   return (
-    <main>
+    <main ref={containerRef}>
         <div>
-            <h1>Login</h1>
+            <h1 className='animate'>Login</h1>
             <form onSubmit={async (e)=>{
                 e.preventDefault()
                 await handleLogin({username,password})
                 navigate('/')
             }}>
-                <div>
+                <div className='animate'>
                     <label htmlFor="username">Username</label>
                     <input 
-                        type="username" 
+                        type="text" 
                         name="username" 
                         placeholder='Enter Your Username'
                         onChange={(e)=>{
@@ -34,7 +49,7 @@ const login = () => {
                         }}
                     />
                 </div>
-                <div>
+                <div className='animate'>
                     <label htmlFor="password">Password</label>
                     <input 
                         type="password" 
@@ -45,11 +60,16 @@ const login = () => {
                         }}
                     />
                 </div>
-                <button>
+                <button className='animate'>
                     Login
                 </button>
             </form>
-            <span>Don't have Account <Link to={"/register"} >Register</Link></span>
+            <span className='animate'>
+                Don't have Account 
+                <Link to={"/register"} >
+                    Register
+                </Link>
+            </span>
         </div>
     </main>
   )
